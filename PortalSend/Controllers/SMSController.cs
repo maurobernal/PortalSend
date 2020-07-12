75535c2869ecbd455c0fd7b5c2dae3d74a39a9b7
mauro_bernal_rios <mauro_bernal_rios@hotmail.com>
2020-07-12 19:22:59 -0300

    MB 20200712 Add Theme Amoeba

--- PortalSend/Controllers/SMSController.cs
+++ PortalSend/Controllers/SMSController.cs
@@ -12,12 +12,14 @@ namespace PortalSend.Controllers
         // GET: SMS
         public ActionResult Index()
         {
+            /*
             ViewData["Resultado1"]=SynWayAPI_models.Consultas(SynWayAPI_models.Consultas_Types.GetPortInfo);
             ViewData["Resultado2"] = SynWayAPI_models.Consultas(SynWayAPI_models.Consultas_Types.GetPortConnectState);
             ViewData["Resultado3"] = SynWayAPI_models.Consultas(SynWayAPI_models.Consultas_Types.GetWirelessinfo);
             ViewData["Resultado4"] = SynWayAPI_models.Consultas(SynWayAPI_models.Consultas_Types.GetDetailPortInfo);
             ViewData["Resultado5"] = SynWayAPI_models.EnvioSMS(SynWayAPI_models.Envios_Types.SendSMS,"2616142278","1","prueba:"+ DateTime.Now.ToShortTimeString());
             ViewData["Resultado6"] = SynWayAPI_models.EnvioSMS(SynWayAPI_models.Envios_Types.SendSMS, "2616142278", "1", "prueba:" + DateTime.Now.ToShortTimeString());
+            */
             return View();
         }
 
