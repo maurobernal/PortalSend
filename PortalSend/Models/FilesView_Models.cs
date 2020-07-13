8025b251ef0529ffa7ad64d7541683d7f2490cf8
mauro_bernal_rios <mauro_bernal_rios@hotmail.com>
2020-07-12 23:12:54 -0300

    MB 20200712 Upload

---
+++ PortalSend/Models/FilesView_Models.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PortalSend.Helpers;
+namespace PortalSend.Models
+{
+    public class FilesView_Models
+    {
+        public ViewDataUploadFilesResult[] Files { get; set; }
+    }
+}
